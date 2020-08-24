990e5bdb08abdb34f63b5328db15ff10c5537dc7
Михаил Кукаркин <wardenclock@gmail.com>
2020-08-24 17:46:29 +0500

    initial commit

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Wardenclock1759/StoreAPI
 
 go 1.15
 
-require github.com/BurntSushi/toml v0.3.1
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/sirupsen/logrus v1.6.0
+)
--- internal/apiserver/config.go
+++ internal/apiserver/config.go
@@ -2,10 +2,12 @@ package apiserver
 
 type Config struct {
 	BindAddress string `toml:"bind_addr"`
+	LogLevel    string `toml:"log_level"`
 }
 
 func NewConfig() *Config {
 	return &Config{
 		BindAddress: ":8080",
+		LogLevel:    "debug",
 	}
 }
